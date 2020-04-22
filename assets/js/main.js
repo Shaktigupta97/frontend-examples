8a131087956f244ce11c2b329426ae74ea24e941
Gaurav Kumar <developergkindia@gmail.com>
2020-04-22 17:58:15 +0530

    added side nav example

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$('#open-side-nav').on('click', function(){
+  // var side_nav_ele = $('#side-nav');
+
+  $('#side-nav').toggleClass('open');
+
+  // if(side_nav_ele.hasClass('open')){
+  //   side_nav_ele.removeClass('open');
+  // } else {
+  //   side_nav_ele.addClass('open');
+  // }
+});
